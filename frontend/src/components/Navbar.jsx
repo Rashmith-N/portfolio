ad723d9af0730066d6e21d924b1b203ea54b1345
Rashmith N <rashmithpoojary22@gmail.com>
2026-09-21 20:00:49 +0530

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <header className="navbar" id="top">
+      <div className="container">
+        <a href="#top" className="navbar-brand">Rashmith N</a>
+        <nav aria-label="Primary">
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
